edd6023d3473115363bcede0b750626c39f0ae63
Marcelo Henrique <mah_henrique4478@hotmail.com>
2024-05-05 18:55:25 -0300

    Testes de funcionalidades da aplicação realizados - (e2e)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { Postagem } from './postagem/entities/postagem.entity';
+
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { UsuarioModule } from './usuario/usuario.module';
       database: 'db_blogpessoal',
       entities: [Postagem, Tema, Usuario],
       synchronize: true,
-      logging: true
+      // logging: true
     }),
     PostagemModule,
     TemaModule,
