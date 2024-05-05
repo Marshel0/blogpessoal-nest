d998633e70df36679973dd6f123ed5c960e25c03
Marcelo Henrique <mah_henrique4478@hotmail.com>
2024-05-05 19:58:40 -0300

    Deploy do Projeto Blog Pessoal

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00' //Arruma o fuso horario
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Marcelo Henrique", "https://github.com/Marshel0", "mah_henrique4478@hotmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document)
+
+  process.env.TZ = '-03:00'; //Arruma o fuso horario
 
   app.useGlobalPipes(new ValidationPipe()); //Cria o construtor e habilita para validações de dados
 
   app.enableCors(); //Habilita para aceitar requisições de servidores de outras origens e não só onde está hospedado 
 
-  await app.listen(4001);
+  await app.listen(process.env.PORT || 4001);
 }
 bootstrap();
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -3,31 +3,38 @@ import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Usuario } from "../../usuario/entities/usuario.entity";
 import { Tema } from "../../tema/entities/tema.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: "tb_postagem"})
 export class Postagem{
 
+    @ApiProperty()
     @PrimaryGeneratedColumn() //Chave primaria e auto_increment
     id: number;
 
+    @ApiProperty()
     @Transform(({value}: TransformFnParams) => value?.trim())
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
     titulo: string;
 
+    @ApiProperty()
     @Transform(({value}: TransformFnParams) => value?.trim())
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
     texto: string;
 
+    @ApiProperty()
     @UpdateDateColumn()
     data: Date;
 
+    @ApiProperty()
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema
     
+    @ApiProperty()
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
